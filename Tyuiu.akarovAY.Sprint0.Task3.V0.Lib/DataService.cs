ee71a441504e1720b4f7fe73d33cfabbfefe6410
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-09-15 20:59:25 +0500

    Task3

---
+++ Tyuiu.akarovAY.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.MakarovAY.Sprint0.Task3.V0.Lib
+
+{
+    public class DataService
+    {
+        //Реализицая метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        } 
+    }
+}
