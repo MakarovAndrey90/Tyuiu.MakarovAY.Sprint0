1fb6499d3cba386aace953253806853cd2ce1aa1
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-09-15 20:05:07 +0500

    Task2

---
+++ Tyuiu.MakarovAY.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MakarovAY.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MakarovAY.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Игорь"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
