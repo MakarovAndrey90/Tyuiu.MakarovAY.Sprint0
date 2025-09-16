03bcfb410ef31eea7d0c7ac3c79cc538d089158a
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-09-16 18:31:42 +0500

    Task7

---
+++ Tyuiu.MakarovAY.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+namespace Tyuiu.MakarovAY.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOnen, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOnen[i] + numTwo[i];
+            }
+            return resultArray;
+        } 
+    }
+}
